99dc7f1b9f9962c49986250521fb33b3fbbae982
Vinay-quadfacetech <vinay.kelkar@quadfacetech.com>
2026-07-22 00:08:32 +0530

    feat: complete Day 002 - data types

---
+++ python/fundamentals/day002_age_calculator.py
@@ -0,0 +1,15 @@
+line = "="*36
+
+print(line)
+print('Project Phoenix - Age Calculator')
+print(line)
+
+name = input('Enter Your name: ')
+age = int(input('Enter Your age: '))
+
+print(f'Hello {name}!')
+
+print(f'Current age : {age}')
+print(f'After 5 years : {age + 5}')
+print(f'After 10 years : {age + 10}')
+print("10"+5)
